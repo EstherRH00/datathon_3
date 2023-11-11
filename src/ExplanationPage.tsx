603b2e68cf8b58229638fbb71ca57f01fd5df5bc
EstherRH00 <eruanohortoneda@gmail.com>
2023-11-11 20:41:27 +0100

    First write

--- src/ExplanationPage.tsx
+++ src/ExplanationPage.tsx
@@ -3,8 +3,16 @@ import React from "react";
 
 function Explanation() {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="m-5 text-primary-color bg-box-color">
+      <h1 className="text-primary-color">How did we make this happen</h1>
+      <p>
+        First of all we dove deep into the data and, using mainly pandas, explored the dataset.
+        <br/>
+        We observe that with the code (CODIGO) and the number of units per pack (UNIDADESCONSUMOCONTENIDAS) we can identify
+        each pack of each item.
+        <br/>
+        The orders are from years 2016-2023
+      </p>
     </div>
   );
 }
