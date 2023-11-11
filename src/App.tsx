0f1b3f0daf80b481c14ef51843d2e8595f8e52e4
EstherRH00 <eruanohortoneda@gmail.com>
2023-11-11 19:57:20 +0100

    Revert "Updates"
    This reverts commit 6d7b04a5ebd8ba74e826443fee0cb214bbe4afc7.

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+// App.tsx
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom"; // Import the Router component
+
+function App() {
+  return (
+    <Router>
+      <h1>My App</h1>
+    </Router>
+  );
+}
+
+export default App;
