15224e478c17ec8ba7399ee1ee5aae63a8e5c5d6
EstherRH00 <eruanohortoneda@gmail.com>
2023-11-11 22:48:00 +0100

    Cosmetic changes

--- src/ExplanationPage.tsx
+++ src/ExplanationPage.tsx
@@ -1,21 +1,49 @@
 // LandingPage.tsx
 import React from "react";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function Explanation() {
   return (
     <div className="m-5 bg-box-color p-5">
       <Header destination={"/datathon_3"} button_text={"Home"}/>
 
-      <h1 className="text-5xl py-2.5">How did we make this happen</h1>
-      <p className="text-ml">
-        First of all we dove deep into the data and, using mainly pandas, explored the dataset.
-        <br/>
-        We observe that with the code (CODIGO) and the number of units per pack (UNIDADESCONSUMOCONTENIDAS) we can identify
-        each pack of each item.
-        <br/>
-        The orders are from years 2016-2023
+      <h1 className="text-3xl py-2.5">Roadmap</h1>
+      <p className="pt-2 text-ml">
+        First of all we dove deep into the data and, using mainly pandas, explored the dataset; we were looking to
+        answer
+        the following questions:
       </p>
+      <ul className="pt-2 pl-4 list-disc marker:text-green-600">
+        <li>How can we give a broad description of the data?</li>
+        <li>Did COVID afect the data? If so, how?</li>
+        <li>Are there any periodic events?</li>
+        <li>What is the shelf life of each product?</li>
+        <li>Are any products co-related?</li>
+      </ul>
+      <p className="pt-2 text-ml">
+        Afterwards we trained [PLACEHOLDER]
+      </p>
+      <hr className="h-px my-8 border-0 bg-green-300"/>
+      <h1 className="text-3xl py-2.5">Data exploration</h1>
+      <p className="pt-2 text-ml">
+        During the data exploration, we found some interesting properties that we want to highlight:
+      </p>
+      <ul className="pt-2 pl-4 list-disc marker:text-green-600">
+        <li>How can we give a broad description of the data?</li>
+      </ul>
+
+      <p className="pt-2 text-ml">
+        And with this insight we decided to add the following columns:
+      </p>
+      <hr className="h-px my-8 border-0 bg-green-300"/>
+      <h1 className="text-3xl py-2.5">Model training and prediction</h1>
+      <p className="pt-2 text-ml">
+        [PLACEHOLDER]
+      </p>
+
+      <Footer />
+
     </div>
   );
 }
@@ -24,7 +52,7 @@ class ExplanationPage extends React.Component {
   render() {
     return (
       <div>
-        <Explanation />
+        <Explanation/>
       </div>
     );
   }
--- src/Header.tsx
+++ src/Header.tsx
@@ -12,9 +12,10 @@ const Header: React.FC<HeaderProps> = ({destination, button_text}: HeaderProps) 
   const navigate = useNavigate()
 
   return (
+    <>
     <div className="w-full grid grid-cols-2 content-center ">
       <div>
-        <h1 className="text-5xl py-2.5">NTT data Challenge</h1>
+        <h1 className="text-green-300 text-5xl py-2.5">NTT data Challenge</h1>
       </div>
       <div className="flex justify-end items-center">
         <button className="p-1 px-5 border border-green-300 rounded text-green-300 items-center justify-center" onClick={()=>navigate(destination)}>
@@ -22,6 +23,9 @@ const Header: React.FC<HeaderProps> = ({destination, button_text}: HeaderProps) 
         </button>
       </div>
     </div>
+
+      <hr className="h-px my-8 border-0 bg-green-300"/>
+      </>
   )
 }
 
